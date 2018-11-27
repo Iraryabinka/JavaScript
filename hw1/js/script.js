37fa566ee17d36cc48febc58dcfefcbeb59cb1c9
Irina Chervyakova <irinachervaykova@gmail.com>
2018-11-27 19:36:20 +0200

    new commit

---
+++ hw1/js/script.js
@@ -0,0 +1,11 @@
+'use strict';   
+
+const adminLogin = 'admin';
+const adminPassword = 'm4ng0h4ckz';
+const userCancel = 'Отменено пользователем!';
+const wrongLogin = 'Доступ запрещен, неверный логин!';
+const wrongPassword = 'Доступ запрещен, неверный пароль!';
+const validPassword = 'Добро пожаловать!';
+
+console.log(userCancel);
+
