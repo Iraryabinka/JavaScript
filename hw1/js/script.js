4e5d9b6b047ebc2c00d31c305987a4b837af780c
Irina Chervyakova <irinachervaykova@gmail.com>
2018-11-30 21:07:14 +0200

    new commit

--- hw1/js/script.js
+++ hw1/js/script.js
@@ -2,10 +2,34 @@
 
 const adminLogin = 'admin';
 const adminPassword = 'm4ng0h4ckz';
+
 const userCancel = 'Отменено пользователем!';
 const wrongLogin = 'Доступ запрещен, неверный логин!';
 const wrongPassword = 'Доступ запрещен, неверный пароль!';
 const validPassword = 'Добро пожаловать!';
+//const enterPassword = 'Please enter your password:'
 
-console.log(userCancel);
+let enterLogin = prompt('Please enter your login:');
+let enterPassword = prompt('Please enter your password:');
+
+if (enterLogin === 'adminLogin') {
+    let enterPassword = prompt('Please enter your password:');
+
+  if (enterPassword === 'adminPassword') {
+        alert(validPassword)
+    
+    } else if (enterPassword === null) {
+        alert(userCancel);
+    
+    } else if (enterPassword !== 'adminLogin') {
+        alert(wrongPassword);
+    }
+
+} else if (enterLogin === null) {
+    alert(userCancel);
+
+} else if (enterLogin !== 'adminLogin') {
+    alert(wrongLogin);
+}
+
 
